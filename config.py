b410b685973b30f538a63b4996db9a71153cc6d9
aydin <aydinjalil@gmail.com>
2020-03-07 11:54:34 -0500

    some work with numpy array

---
+++ config.py
@@ -0,0 +1,4 @@
+  consumer_key = 'GX4poS0wn1qph7B3BZqIXasUS'
+  consumer_secret = 'CHDP3vCUiQEr8gzdJJdXO5QpDNO9r2Ibs0SlZvWNiXDTSHejQk'
+  access_token = '2806560046-pf15qnQ1Hb9nAQ9mQ3kwphStBvQALVusN7BDHlU'
+  access_token_secret = '3tJ10g6rZ2NwWi92SgH2YZY1BW1V7lnBeZGG9MUafkJnw'
